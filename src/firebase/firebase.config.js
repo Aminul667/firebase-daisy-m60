e438f31751704d21046871f197b5eda2ba8fb81d
Aminul667 <rahathssn@gmail.com>
2023-04-18 18:53:21 +0600

    first setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiD7rW5S2KL2lsv4LG85HaeKK_ocLrwdk",
+  authDomain: "fir-daisy-m60.firebaseapp.com",
+  projectId: "fir-daisy-m60",
+  storageBucket: "fir-daisy-m60.appspot.com",
+  messagingSenderId: "1075532973748",
+  appId: "1:1075532973748:web:d3964d755caf5a9bfdb9e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
