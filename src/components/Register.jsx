040b88bd1ded17ece4545d4afcd55d522e2758fd
Aminul667 <rahathssn@gmail.com>
2023-04-19 16:33:38 +0600

    60-5 Add Create user and Sign in user using context API

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,23 +1,33 @@
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../providers/AuthProviders';
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProviders";
 
 const Register = () => {
+  const { user, createUser } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext);
-    console.log(user);
+  const handleRegister = (event) => {
+    event.preventDefault();
 
-    const handleRegister = event => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(name, email, password);
-    }
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
+    console.log(name, email, password);
+
+    createUser(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col">
         <div className="text-center">
           <h1 className="text-5xl font-bold">Please Register now!</h1>
@@ -66,13 +76,13 @@ const Register = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary">Register</button>
             </div>
           </form>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Register;
